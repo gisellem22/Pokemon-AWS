eafb3057441629ec88fab4bdf99b0677a9707e80
gsdossantos <gsdossantos@sodimac.cl>
2021-03-29 01:04:02 -0300

    add: cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,16 +9,29 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 export class HomeComponent implements OnInit {
 
   public loading = true;
-  pokeImg: string;
+  pokeList: any[];
+  pokeUrlList = [];
 
   constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
 
-    this.pokemonService.getPokemon('pikachu').subscribe(res => {
+    this.pokemonService.getPokemonList().subscribe(res => {
       console.log('pokemon ---->', res);
-      this.pokeImg = res.sprites.versions['generation-v']['black-white'].animated['front_default'];
-      console.log('img ---->', this.pokeImg);
+      this.pokeList = res.results;
+      console.log('img ---->', this.pokeList);
+      this.pokeList.forEach(poke => {
+        this.pokemonService.getPokemon(poke.name).subscribe(pokemon => {
+          this.pokeUrlList.push(
+            {
+              name: poke.name,
+              url: pokemon.sprites.versions['generation-v']['black-white'].animated['front_default']
+            }
+          )
+          console.log('img ---->', this.pokeList);
+          // this.pokeImg = res.sprites.versions['generation-v']['black-white'].animated['front_default'];
+        })
+      })
     })
   }
 
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -8,10 +8,15 @@ import { Observable } from 'rxjs';
 export class PokemonService {
 
   url: string = 'https://pokeapi.co/api/v2/pokemon/';
+  url2: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=24';
 
   constructor(private http: HttpClient) { }
 
   getPokemon(name: string): Observable<any> {
     return this.http.get<any>(this.url + name);
+  }
+
+  getPokemonList(): Observable<any> {
+    return this.http.get<any>(this.url2);
   }
 }
